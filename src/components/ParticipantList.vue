fa8ec264926ad75a85e5eb19b79b32666c3db223
Ahmosys <ahmosyspro@protonmail.com>
2023-11-26 22:23:19 +0100

    feat: split participant list to home view

--- src/components/ParticipantList.vue
+++ src/components/ParticipantList.vue
@@ -1,42 +1,17 @@
 <template>
-  <v-container class="fill-height">
-    <v-responsive class="align-center fill-height">
-      <v-row class="text-center">
-        <participant-remaining-time-section />
-        <v-divider inset vertical></v-divider>
-        <participant-remaining-section />
-        <v-divider inset vertical></v-divider>
-        <participant-last-selected-section />
-      </v-row>
-      <v-row class="text-center">
-        <v-col
-          v-for="participant in storeParticipants.getParticipants"
-          :key="participant.id"
-          cols="2"
-        >
-          <participant-card :participant="participant" />
-        </v-col>
-      </v-row>
-      <v-row class="text-center">
-        <v-col cols="12">
-          <participant-selection-button @select="selectRandomParticipant" />
-        </v-col>
-      </v-row>
-    </v-responsive>
-  </v-container>
+  <v-col
+    v-for="participant in storeParticipants.getParticipants"
+    :key="participant.id"
+    cols="2"
+  >
+    <participant-card :participant="participant" />
+  </v-col>
 </template>
 
 <script setup>
 import { useParticipantStore } from "@/store/participant";
 
 import ParticipantCard from "./ParticipantCard.vue";
-import ParticipantSelectionButton from "./ParticipantSelectionButton.vue";
-import ParticipantRemainingSection from "./ParticipantRemainingSection.vue";
-import ParticipantLastSelectedSection from "./ParticipantLastSelectedSection.vue";
-import ParticipantRemainingTimeSection from "./ParticipantRemainingTimeSection.vue";
 
 const storeParticipants = useParticipantStore();
-
-const selectRandomParticipant = () =>
-  storeParticipants.selectRandomParticipant();
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,36 @@
 <template>
-  <participant-list />
+  <v-container class="fill-height">
+    <v-responsive class="align-center fill-height">
+      <v-row class="text-center">
+        <participant-remaining-time-section />
+        <v-divider inset vertical></v-divider>
+        <participant-remaining-section />
+        <v-divider inset vertical></v-divider>
+        <participant-last-selected-section />
+      </v-row>
+      <v-row class="text-center">
+        <participant-list />
+      </v-row>
+      <v-row class="text-center">
+        <v-col cols="12">
+          <participant-selection-button @select="selectRandomParticipant" />
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
 </template>
 
 <script setup>
-  import ParticipantList from '@/components/ParticipantList.vue'
+import { useParticipantStore } from "@/store/participant";
+
+import ParticipantList from "@/components/ParticipantList.vue";
+import ParticipantSelectionButton from "@/components/ParticipantSelectionButton.vue";
+import ParticipantRemainingSection from "@/components/ParticipantRemainingSection.vue";
+import ParticipantLastSelectedSection from "@/components/ParticipantLastSelectedSection.vue";
+import ParticipantRemainingTimeSection from "@/components/ParticipantRemainingTimeSection.vue";
+
+const storeParticipants = useParticipantStore();
+
+const selectRandomParticipant = () =>
+  storeParticipants.selectRandomParticipant();
 </script>
