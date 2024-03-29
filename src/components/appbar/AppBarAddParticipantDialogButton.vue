c77c249e15bf8655d31b1cd19e2b63fcd30a7182
Ahmosys <ahmosyspro@protonmail.com>
2024-03-29 22:29:18 +0100

    refactor: split component in child component

---
+++ src/components/appbar/AppBarAddParticipantDialogButton.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-dialog 
+    v-model="dialogIsOpened"
+    max-width="600"
+  >
+    <template #activator="{ props: activatorProps }">
+      <v-btn 
+        class="text-none font-weight-regular mr-2" 
+        prepend-icon="mdi-account-multiple-plus"
+        text="Add participant" 
+        variant="tonal" 
+        v-bind="activatorProps"
+        :disabled="storeParticipants.isCounting" 
+      />
+    </template>
+    <v-card 
+      title="Add a new participant" 
+      prepend-icon="mdi-account-multiple-plus"
+    >
+      <v-card-text>
+        <v-row dense>
+          <v-col cols="12">
+            <v-text-field 
+              v-model="firstName" 
+              label="First name*" 
+              placeholder="John"
+              :rules="[rules.required]" 
+              maxlength="10" 
+              variant="underlined"
+              required 
+              counter 
+            />
+          </v-col>
+          <v-col cols="12">
+            <v-text-field 
+              v-model="trigram" 
+              label="Trigram*" 
+              placeholder="jdo" 
+              :rules="[rules.required]"
+              maxlength="3" 
+              variant="underlined" 
+              counter 
+              required 
+            />
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-divider />
+      <v-card-actions>
+        <v-spacer />
+        <v-btn 
+          text="Close" 
+          variant="plain"
+          @click="setIsOpened" 
+        />
+        <v-btn 
+          color="primary" 
+          text="Save" 
+          variant="tonal" 
+          @click="handleAddParticipant" 
+        />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+import { useParticipantStore } from "@/store/participant";
+
+const storeParticipants = useParticipantStore();
+
+const dialogIsOpened = ref(false);
+const firstName = ref("");
+const trigram = ref("");
+const rules = {
+  required: value => !!value || 'Field is required',
+};
+
+const setIsOpened = () => {
+  dialogIsOpened.value = !dialogIsOpened.value;
+};
+
+const resetTextFields = () => {
+  firstName.value = "";
+  trigram.value = "";
+};
+
+const handleAddParticipant = () => {
+  storeParticipants.addParticipant(firstName.value, trigram.value);
+  setIsOpened();
+  resetTextFields();
+};
+</script>
--- src/layouts/default/DefaultAppBar.vue
+++ src/layouts/default/DefaultAppBar.vue
@@ -6,33 +6,7 @@
       Daily Scrum Picker
     </v-app-bar-title>
     <!-- Add participant dialog -->
-    <v-dialog v-model="dialogAddParticipant" max-width="600">
-      <template v-slot:activator="{ props: activatorProps }">
-        <v-btn class="text-none font-weight-regular mr-2" prepend-icon="mdi-account-multiple-plus"
-          text="Add participant" variant="tonal" v-bind="activatorProps"
-          :disabled="storeParticipants.isCounting"></v-btn>
-      </template>
-      <v-card title="Add a new participant" prepend-icon="mdi-account-multiple-plus">
-        <v-card-text>
-          <v-row dense>
-            <v-col cols="12">
-              <v-text-field v-model="firstName" label="First name*" placeholder="John"
-                :rules="[rules.required]" maxlength="10" variant="underlined" required counter></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field v-model="trigram" label="Trigram*" placeholder="jdo" :rules="[rules.required]"
-                maxlength="3" variant="underlined" counter required></v-text-field>
-            </v-col>
-          </v-row>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn text="Close" variant="plain" @click="dialogAddParticipant = false"></v-btn>
-          <v-btn color="primary" text="Save" variant="tonal" @click="handleAddParticipant"></v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <app-bar-add-participant-dialog-button />
     <!-- Theme switcher -->
     <v-btn :icon="theme.global.current.value.dark
       ? 'mdi-weather-night'
@@ -61,18 +35,9 @@
 import { ref } from "vue";
 import { useTheme } from "vuetify";
 
-import { useParticipantStore } from "@/store/participant";
-
-const storeParticipants = useParticipantStore();
-
+import AppBarAddParticipantDialogButton from "@/components/appbar/AppBarAddParticipantDialogButton.vue";
+ 
 const dialogInfo = ref(false);
-const dialogAddParticipant = ref(false);
-
-const firstName = ref("");
-const trigram = ref("");
-const rules = {
-  required: value => !!value || 'Field is required',
-};
 
 const theme = useTheme();
 
@@ -82,12 +47,5 @@ const handleThemeState = () => {
   const newTheme = theme.global.current.value.dark ? "light" : "dark";
   theme.global.name.value = newTheme;
   localStorage.setItem("userTheme", newTheme);
-};
-
-const handleAddParticipant = () => {
-  storeParticipants.addParticipant(firstName.value, trigram.value);
-  dialogAddParticipant.value = false;
-  firstName.value = "";
-  trigram.value = "";
 };
 </script>
