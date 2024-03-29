3330e412b948542757538b2186ddc2ea1995c260
Ahmosys <ahmosyspro@protonmail.com>
2024-03-29 22:52:45 +0100

    refactor: split component in child component

---
+++ src/components/appbar/AppBarThemeSwitcherButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-btn 
+    :icon="theme.global.current.value.dark
+      ? 'mdi-weather-night'
+      : 'mdi-weather-sunny'"
+    @click="handleThemeState" 
+  />
+</template>
+
+<script setup>
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+
+const handleThemeState = () => {
+  const newTheme = theme.global.current.value.dark ? "light" : "dark";
+  theme.global.name.value = newTheme;
+  localStorage.setItem("userTheme", newTheme);
+};
+</script>
--- src/layouts/default/DefaultAppBar.vue
+++ src/layouts/default/DefaultAppBar.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-app-bar flat scroll-behavior="hide">
+  <v-app-bar 
+    flat 
+    scroll-behavior="hide"
+  >
     <!-- Title -->
     <v-app-bar-title>
       <v-icon icon="mdi-account-check" />
@@ -8,28 +11,16 @@
     <!-- Add participant dialog -->
     <app-bar-add-participant-dialog-button />
     <!-- Theme switcher -->
-    <v-btn 
-      :icon="theme.global.current.value.dark
-        ? 'mdi-weather-night'
-        : 'mdi-weather-sunny'"
-      @click="handleThemeState" 
-    />
+    <app-bar-theme-switcher-button />
     <!-- Info dialog -->
     <app-bar-info-dialog-button />
   </v-app-bar>
 </template>
 
 <script setup>
-import { useTheme } from "vuetify";
 
 import AppBarAddParticipantDialogButton from "@/components/appbar/AppBarAddParticipantDialogButton.vue";
 import AppBarInfoDialogButton from "@/components/appbar/AppBarInfoDialogButton.vue";
+import AppBarThemeSwitcherButton from "@/components/appbar/AppBarThemeSwitcherButton.vue";
 
-const theme = useTheme();
-
-const handleThemeState = () => {
-  const newTheme = theme.global.current.value.dark ? "light" : "dark";
-  theme.global.name.value = newTheme;
-  localStorage.setItem("userTheme", newTheme);
-};
 </script>
