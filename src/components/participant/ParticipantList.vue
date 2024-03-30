7ce0a50c0646681cacf8f068b3b8c4fd9d7dab0f
Ahmosys <ahmosyspro@protonmail.com>
2024-03-30 23:21:47 +0100

    feat: add gsap animation

--- src/components/participant/ParticipantList.vue
+++ src/components/participant/ParticipantList.vue
@@ -1,17 +1,52 @@
 <template>
-  <v-col
-    v-for="participant in storeParticipants.getParticipants"
-    :key="participant.id"
-    cols="2"
+  <transition-group 
+    :css="false" 
+    appear
+    @enter="participantCardOnEnter" 
   >
-    <participant-list-card :participant="participant" />
-  </v-col>
+    <v-col
+      v-for="(participant, index) in storeParticipants.getParticipants"
+      :key="participant.id"
+      :data-index="index"
+      cols="2"
+    >
+      <participant-list-card :participant="participant" />
+    </v-col>
+  </transition-group> 
 </template>
 
 <script setup>
+import { ref } from "vue";
+
 import { useParticipantStore } from "@/store/participant";
 
 import ParticipantListCard from "@/components/participant/ParticipantListCard.vue";
 
+import gsap from "gsap";
+
 const storeParticipants = useParticipantStore();
+
+const isFirstLoad = ref(true);
+
+const participantCardOnEnter = (el, done) => {
+  gsap.fromTo(
+    el,
+    {
+      opacity: 0,
+      translateY: -30,
+    },
+    {
+      opacity: 1,
+      translateY: 0,
+      duration: 1,
+      delay: isFirstLoad.value ? el.dataset.index * 0.2 : 0,
+      onComplete: () => {
+        if (isFirstLoad.value) {
+          isFirstLoad.value = false;
+        }
+        done();
+      }
+    }
+  )
+}
 </script>
